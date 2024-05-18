8068283112867bb273656b4759dbd79f84360489
AlexandeOliveira12 <lebella321@gmail.com>
2024-05-18 14:32:30 -0300

    Orientação a objeto

--- ProjetoConta/src/Conta.java
+++ ProjetoConta/src/Conta.java
@@ -1,6 +1,15 @@
 public class Conta {
   private String numeroCont;
   private Cliente titular;
+  private Dependente[] dependente;
+
+  public Dependente[] getDependente() {
+    return dependente;
+  }
+
+  public void setDependente(Dependente[] dependente) {
+    this.dependente = dependente;
+  }
 
   public String getNumeroCont() {
     return numeroCont;
--- ProjetoConta/src/TestaConta.java
+++ ProjetoConta/src/TestaConta.java
@@ -1,12 +1,28 @@
 public class TestaConta {
 
   public static void main(String[] args) {
+
+    //Criação clientes
     Cliente cliente01 = new Cliente();
     cliente01.setCodigo(1);
     cliente01.setNome("Alexandre");
 
+    //Criação dependentes
+    Dependente dep01 = new Dependente();
+    dep01.setCodigo(1);
+    dep01.setNome("Alexandre");
+    Dependente dep02 = new Dependente();
+    dep02.setCodigo(2);
+    dep02.setNome("Gustavo");
+
+    Dependente[] dependentes = new Dependente[2];
+    dependentes[0] = dep01;
+    dependentes[1] = dep02;
+
+    //Criação conta
     Conta conta01 = new Conta();
     conta01.setNumeroCont("12345678");
     conta01.setTitular(cliente01);
+
   }
 }
